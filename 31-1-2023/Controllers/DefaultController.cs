13c86ed554d2d6f2d0e4a6f020c0c53afbd584cd
Mohammad-Sawalha98 <115452622+Mohammad-Sawalha98@users.noreply.github.com>
2023-01-31 23:59:12 -0800

    Task1

---
+++ 31-1-2023/Controllers/DefaultController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace _31_1_2023.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public string Index()
+        {
+            return "<a href='/hh.jpg' download><img src='/hh.jpg'></a>";
+        }
+
+        public string About()
+        {
+            return "This is About us page";
+        }
+
+        public string Contact()
+        {
+            return "This is Contact us page";
+        }
+    }
+}
---
+++ 31-1-2023/Controllers/sawalhaController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace _31_1_2023.Controllers
+{
+    public class sawalhaController : Controller
+    {
+        // GET: sawalha
+        public int Age()
+        {
+            return 25;
+        }
+
+        public string FullName()
+        {
+            return "Mohammad Sawalha";
+        }
+
+        public ActionResult Hoppies()
+        {
+            return Content("<head><link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC\" crossorigin=\"anonymous\"><script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM\" crossorigin=\"anonymous\"></script></head><table class='table'><tr><th  class=\"table-dark\" >hopy name</th><th>my age</th><tr/><tr><td>football</td><td>10 years</td><tr/><tr><td>swimming</td><td>from 15-25 yers</td><tr/><table/>");
+        }
+    }
+}
