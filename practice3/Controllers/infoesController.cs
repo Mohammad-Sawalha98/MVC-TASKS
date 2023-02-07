3f4d99aa7630535ee829a1eecbaf34c6c509e6b4
Mohammad-Sawalha98 <115452622+Mohammad-Sawalha98@users.noreply.github.com>
2023-02-06 16:23:00 -0800

    Task-1//6-Feb

---
+++ practice3/Controllers/infoesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using practice3.Models;
+
+namespace practice3.Controllers
+{
+    public class infoesController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: infoes
+        public ActionResult Index()
+        {
+            return View(db.infoes.ToList());
+        }
+
+        public ActionResult test()
+        {
+            var soso = db.infoes.FirstOrDefault();
+            return PartialView("test", soso);
+        }
+
+        // GET: infoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            info info = db.infoes.Find(id);
+            if (info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(info);
+        }
+
+        // GET: infoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: infoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,First_Name,Last_Name,E_mail,Phone,Age,Job_Title,Gender,Image,CV")] info info)
+        {
+            if (ModelState.IsValid)
+            {
+                db.infoes.Add(info);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(info);
+        }
+
+        // GET: infoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            info info = db.infoes.Find(id);
+            if (info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(info);
+        }
+
+        // POST: infoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,First_Name,Last_Name,E_mail,Phone,Age,Job_Title,Gender,Image,CV")] info info)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(info).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(info);
+        }
+
+        // GET: infoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            info info = db.infoes.Find(id);
+            if (info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(info);
+        }
+
+        // POST: infoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            info info = db.infoes.Find(id);
+            db.infoes.Remove(info);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
